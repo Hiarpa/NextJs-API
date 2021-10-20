cf4ff82d4e0c0389506d3ebacee6112a0aa64372
Hiarpa Neto <hiarpanetto@gmail.com>
2021-10-20 18:39:51 -0400

    mongodb

---
+++ src/utils/mongodb.ts
@@ -0,0 +1,10 @@
+import { MongoClient } from 'mongodb'
+
+const uri = process.env.MONGODB_URI
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}
+
+let client
+let clientPromise
